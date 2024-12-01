5588b1711b7a7d4b69f7fc37461f793c01f85ebf
Maryiamreda <maryiamreda@gmail.com>
2024-12-01 19:44:31 +0200

    responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,20 @@ import Arrow from '../assets/assets_frontend/arrow_icon.svg'
 import Grouprofile from '../assets/assets_frontend/group_profiles.png'
 const Header = () => {
     return (
-        <div className="bg-primary px-20 h-[78vh] flex mt-5 rounded-lg">
-            <div className=' flex flex-col gap-5 pt-32'>
-                <p className='text-white md:text-5xl font-semibold  text-start leading-snug'>Book Appointment<br />
+        <div className="bg-primary px-10 md:px-20 md:h-[78vh]  md:flex md:flex-row flex-col  mt-5 rounded-lg">
+            <div className=' flex flex-col items-center md:relative md:top-24  md:items-start gap-5  pt-7 '>
+                <p className='text-white text-3xl md:text-5xl font-semibold  text-start md:leading-tight'>Book Appointment<br />
                     With Trusted Doctors</p>
-                <div className='flex gap-3'>
+                <div className='flex flex-col items-center md:flex-row gap-3'>
                     <img src={Grouprofile} className='w-32 h-12' />
                     <div className='text-white text-sm text-start'>Simply browse through our extensive list of trusted
                         doctors,   <br />
                         schedule your appointment hassle-free.</div>
                 </div>
-                <div className='flex text-gray-600 bg-white text-sm w-48 justify-center p-2 items-center gap-2 rounded-3xl'><p>Book appointment</p><img src={Arrow} className='w-3' /></div>
+                <a href='#speciality' className='flex text-gray-600 bg-white text-sm w-48 justify-center px-2 py-3 items-center gap-2 rounded-3xl'><p>Book appointment</p><img src={Arrow} className='w-3' /></a>
             </div>
-            <div className='flex items-end '>
-                <img src={HeaderImg} className='w-[30rem]' alt="" />
+            <div className='flex items-end md:flex-end '>
+                <img src={HeaderImg} className='w-96 md:w-[32rem]' alt="" />
             </div>
 
         </div>);
