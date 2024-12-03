31f6b9efdb449f75155f8d6ba959d0a296b03962
Maryiamreda <maryiamreda@gmail.com>
2024-12-04 01:14:04 +0200

    displying days

--- src/pages/Appointment.tsx
+++ src/pages/Appointment.tsx
@@ -7,27 +7,32 @@ import Info from '../assets/assets_frontend/info_icon.svg';
 
 const Appointments = () => {
     const { doctorId } = useParams<{ doctorId: string }>();
-
+    const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     const [doctor, setDoctor] = useState<Doctor | null>(null)
     const [doctorSlots, setDoctorSlots] = useState<Array<{ datetime: Date; time: string }[]>>([]);
     const [slotIndex, setSlotIndex] = useState(0)
     const [slotTime, setSlotTime] = useState('')
     const getAvailableSlots = async () => {
         setDoctorSlots([])
-        let today = new Date();
-        let allSlots = []; // Initialize an array to hold all the slots for 7 days
+
+        let today = new Date(); //The new Date() constructor creates a new Date object.
 
         for (let i = 0; i < 7; i++) {
             let currentDate = new Date(today)
-            currentDate.setDate(today.getDate() + i)
+            currentDate.setDate(today.getDate() + i) //getDate() returns the day of the month (from 1 to 31) of a date:
+            //setDate() sets the day of the month of a date.
+
             //setting end time of the date with index
             let endTime = new Date();
             endTime.setDate(today.getDate() + i)
-            endTime.setHours(21, 0, 0, 0)
+            endTime.setHours(21, 0, 0, 0) //setHours() can set the hour, the minute, and the second of a date object:
+
             //setting hours
             if (today.getDate() === currentDate.getDate()) {
                 currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
                 currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+                // the start time is adjusted to be no earlier than 10:00 AM.
+                // If the current time is already past 10:00 AM, it moves to the next available hour.
 
             } else {
                 currentDate.setHours(10)
@@ -38,6 +43,7 @@ const Appointments = () => {
             let timeSlots = []
             while (currentDate < endTime) {
                 let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+                //returns the time portion of a date object as a string, using locale conventions.
                 timeSlots.push({
                     datetime: new Date(currentDate),
                     time: formattedTime
@@ -58,11 +64,9 @@ const Appointments = () => {
     }, [doctorId]);
 
     useEffect(() => { getAvailableSlots() }, [doctor])
-
+    useEffect(() => { console.log(doctorSlots) }, [doctorSlots])
     return (
         <div>
-
-
             <div className="mt-5">
                 <div className="flex gap-5 ">
                     <img src={doctor?.image} className="bg-primary sm:max-w-72 rounded-lg " alt={doctor?.name} />
@@ -86,7 +90,17 @@ const Appointments = () => {
 
                     </div>
                 </div>
-                <div></div>
+                <div>
+                    <h3>Booking slots</h3>
+                    <div>
+                        {doctorSlots.length && doctorSlots.map((item, index) => (
+                            <div key={index}>
+                                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                                <p>{item[0] && item[0].datetime.getDate()}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
 
             </div>
         </div>);
