061e9dd1e4120daf5c8dd60342894b0f6c1a8c45
Maryiamreda <maryiamreda@gmail.com>
2024-12-05 04:13:42 +0200

    fix banner reactivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Arrow from '../assets/assets_frontend/arrow_icon.svg'
 import Grouprofile from '../assets/assets_frontend/group_profiles.png'
 const Header = () => {
     return (
-        <div className="bg-primary px-10 md:px-20 md:h-[78vh]  md:flex md:flex-row flex-col  mt-5 rounded-lg">
-            <div className=' flex flex-col items-center md:relative md:top-24  md:items-start gap-5  pt-7 '>
+        <div className="bg-primary px-10  justify-between md:px-20 md:pt-48    flex md:flex-row  flex-col  mt-5 rounded-lg">
+            <div className=' flex flex-col  md:relative md:bottom-20 gap-5  pt-7 '>
                 <p className='text-white text-3xl md:text-5xl font-semibold  text-start md:leading-tight'>Book Appointment<br />
                     With Trusted Doctors</p>
                 <div className='flex flex-col items-center md:flex-row gap-3'>
@@ -15,8 +15,8 @@ const Header = () => {
                 </div>
                 <a href='#speciality' className='flex text-gray-600 bg-white text-sm w-48 justify-center px-2 py-3 items-center gap-2 rounded-3xl'><p>Book appointment</p><img src={Arrow} className='w-3' /></a>
             </div>
-            <div className='flex items-end md:flex-end '>
-                <img src={HeaderImg} className='w-96 md:w-[32rem]' alt="" />
+            <div className='md:w-1/2 relative '>
+                <img src={HeaderImg} className='w-full md:absolute bottom-0 h-auto rounded-lg' alt="" />
             </div>
 
         </div>);
