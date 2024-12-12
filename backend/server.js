7340e299e2f7d46534190ad159a1731d371a52c3
Maryiamreda <maryiamreda@gmail.com>
2024-12-12 20:26:13 +0200

    Create Backend Server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb';
+import connectCloudinary from './config/cloudinary';
 //app config
 const app = express();
 const port = process.env.PORT || 4000
 app.use(express.json())
+connectDB();
+connectCloudinary()
 
 app.use(cors()) //allow backend to connect with the fronyend 
+//api endpoints
+app.get('/', (req, res) => {
+    res.send('API WORKING')
+
+})
+app.listen(port, () => console.log("Server Start"))
