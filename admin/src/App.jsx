dfadce9a54ffdc66a41de35e7b1d9ab4054fff3a
Maryiamreda <maryiamreda@gmail.com>
2024-12-23 22:10:46 +0200

    Admin Panel sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,12 +7,29 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AppContext } from './context/appContext';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/navBar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 function App() {
   const { aToken } = useContext(AdminContext)
   return aToken ? (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+
+        </Routes>
+      </div>
     </div>
   ) : (<div>
     <Login />
