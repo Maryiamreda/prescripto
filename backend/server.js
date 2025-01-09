157f5b8ef1db5d982d103519b9cf552a8f7a3352
Maryiamreda <maryiamreda@gmail.com>
2025-01-09 21:49:03 +0200

    Create Patient Login and Registration system

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
 import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
 //app config
 const app = express();
 const port = process.env.PORT || 4000
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 })
 app.use('/api/admin', adminRouter);
 app.use('/api/doctor', doctorRouter);
+app.use('/api/user', userRouter);
 
 //All routes defined in adminRouter will be prefixed with /api/admin
 //For example, when you defined adminRouter.post('/add-doctor',...), the full route becomes:/api/admin/add-doctor
